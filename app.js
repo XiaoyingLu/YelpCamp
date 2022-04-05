e66476d126c643a1e628208e4a29f30d21481d83
Amber Lu <amber.lu@ubisoft.com>
2022-04-05 11:16:39 -0300

    serve static assets

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
