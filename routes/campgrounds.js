c6857853cab840d12635bb6b6c40044efc71a80e
Amber Lu <amber.lu@ubisoft.com>
2022-04-05 16:02:57 -0300

    restructured Routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,39 +4,33 @@ const catchAsync = require("../utils/catchAsync");
 const campgrounds = require("../controllers/campgrounds");
 const { isLoggedIn, validateCampground, isAuthor } = require("../middleware");
 
-router.get("/", isLoggedIn, catchAsync(campgrounds.index));
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  .post(
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.post(
-  "/",
-  isLoggedIn,
-  validateCampground,
-  catchAsync(campgrounds.createCampground)
-);
-
-router.get("/:id", isLoggedIn, catchAsync(campgrounds.showCampground));
+router
+  .route("/:id")
+  .get(isLoggedIn, catchAsync(campgrounds.showCampground))
+  .put(
+    isLoggedIn,
+    isAuthor,
+    validateCampground,
+    catchAsync(campgrounds.updateCampground)
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get(
   "/:id/edit",
   isLoggedIn,
   isAuthor,
   catchAsync(campgrounds.renderEditForm)
-);
-
-router.put(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  validateCampground,
-  catchAsync(campgrounds.updateCampground)
-);
-
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  catchAsync(campgrounds.deleteCampground)
 );
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,20 +4,21 @@ const catchAsync = require("../utils/catchAsync");
 const passport = require("passport");
 const users = require("../controllers/users");
 
-router.get("/register", users.renderRegister);
+router
+  .route("/register")
+  .get(users.renderRegister)
+  .post(catchAsync(users.register));
 
-router.post("/register", catchAsync(users.register));
-
-router.get("/login", users.renderLogin);
-
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    failureFlush: true,
-    failureRedirect: "/login",
-  }),
-  catchAsync(users.login)
-);
+router
+  .route("/login")
+  .get(users.renderLogin)
+  .post(
+    passport.authenticate("local", {
+      failureFlush: true,
+      failureRedirect: "/login",
+    }),
+    catchAsync(users.login)
+  );
 
 router.get("/logout", users.logout);
 
