5c1ce5fd86d5b94a84ef9cece8bc3237a6a21667
Amber Lu <amber.lu@ubisoft.com>
2022-04-06 13:25:55 -0300

    improved security

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
 
 const ExpressError = require("./utils/ExpressError");
 const User = require("./models/user");
@@ -38,19 +40,86 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
 
 const sessionConfig = {
+  name: "session",
   secret: "thisshouldbeabettersecret!",
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
 app.use(session(sessionConfig));
 app.use(flash());
+
+const scriptSrcUrls = [
+  "https://cdn.jsdelivr.net/",
+  "https://cdn.jsdelivr.net",
+  "https://stackpath.bootstrapcdn.com",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://api.mapbox.com/mapbox-gl-js/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://cdn.jsdelivr.net/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.mapbox.com/mapbox-gl-js/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
+];
+const fontSrcUrls = ["https://kit.fontawesome.com/"];
+
+app.use(
+  helmet({
+    crossOriginResourcePolicy: { policy: "cross-origin" },
+    crossOriginEmbedderPolicy: false,
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        connectSrc: ["'self'", ...connectSrcUrls],
+        scriptSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          "https://stackpath.bootstrapcdn.com/",
+          ...scriptSrcUrls,
+        ],
+        styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+        workerSrc: ["'self'", "blob:"],
+        objectSrc: [],
+        imgSrc: [
+          "'self'",
+          "blob:",
+          "data:",
+          "https://images.unsplash.com/",
+          "https://res.cloudinary.com/douqbebwk/",
+          "https://res.cloudinary.com/dxeotlho6/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+        ],
+        fontSrc: ["self", ...fontSrcUrls],
+      },
+    },
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -64,12 +133,6 @@ app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
-});
-
-app.get("/fakeUser", async (req, res) => {
-  const user = new User({ email: "test@gmail.com", username: "test" });
-  const newUser = await User.register(user, "chicken");
-  res.send(newUser);
 });
 
 app.use("/", userRoutes);
